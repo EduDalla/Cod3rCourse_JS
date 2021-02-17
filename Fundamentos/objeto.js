9d0640e8c7c5eb807f4f231106ba4dd38f6c3af7
Eduardo Dallabella Lima <62569881+EduDalla@users.noreply.github.com>
2021-02-17 19:38:24 -0300

    Added and changed

--- Fundamentos/objeto.js
+++ Fundamentos/objeto.js
@@ -1,9 +1,6 @@
 const prod1 = {} 
-
 prod1.nome = 'Celular Ultra Mega'
-
 prod1.preco = 4998.90 
-
 Prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço 
 
  
@@ -12,30 +9,19 @@ console.log(prod1)
 
  
 
-const prod2 = { 
+const prod2 = {
 
-    nome: 'Camisa Polo'
+        nome: "Camisa Polo",
+        preco: 79.90,
+        obj: {
+              Blabla: 1,
+        Obj: { 
+              Blabla: 2 
+                }
+        } 
 
-    ,preco: 79.90, 
-
-    obj: { 
-
-           Blabla: 1 
-
-           ,Obj: { 
-
-                   Blabla: 2 
-
-           } 
-
-      } 
-
-} 
-
- 
+}
 
 Prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço 
 
 Console.log(prod2) 
-
-  
